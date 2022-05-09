34dc259e6e611311acc7d5996aa5f6349e71d6b8
Ashwin Kumar <meetashwin2000@gmail.com>
2022-05-09 12:08:56 +0530

    master update

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppheadComponent } from './component/apphead/apphead.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppheadComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
